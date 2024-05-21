f9d60ba89a5fb1df0aab26567ff75011c5c1abd0
Praveen007bot <praveennani963@gmail.com>
2024-05-21 19:30:33 +0530

    socketio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,38 @@ import Signup from "./components/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import { useDispatch, useSelector } from "react-redux";
+import io from 'socket.io-client'
+import { useEffect } from "react";
+import { setSocket } from "./redux/socketSlice";
+import { setOnlineUsers } from "./redux/userSlice";
+
 function App() {
+  const { authUser } = useSelector((store) => store.user);
+  const { socket } = useSelector((store) => store.socket);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (authUser) {
+      const socketio = io("http://localhost:8000", {
+        query: {
+          userId: authUser._id,
+        },
+      });
+      dispatch(setSocket(socketio));
+
+      socketio?.on("getOnlineUsers", (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
+      });
+      return () => socketio.close();
+    } else {
+      if (socket) {
+        socket.close();
+        dispatch(setSocket(null));
+      }
+    }
+  }, [authUser]);
+
   return (
     <div className="App h-screen flex items-center justify-center text-black">
       <BrowserRouter>
