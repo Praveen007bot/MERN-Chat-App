8b6872cca493ade5b5b6e5a573861c1a596c9c36
Praveen007bot <praveennani963@gmail.com>
2024-05-22 12:09:31 +0530

    realtime messages using socketio

--- backend/controllers/MessageController.js
+++ backend/controllers/MessageController.js
@@ -1,5 +1,6 @@
 import { Conversation } from "../models/conversationModel.js";
 import { Message } from "../models/messageModel.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 export const sendMessage = async (req, res) => {
   try {
     const senderId = req.id;
@@ -19,8 +20,12 @@ export const sendMessage = async (req, res) => {
     if (newMessage) {
       getConversation.messages.push(newMessage._id);
     }
+    await Promise.all([getConversation.save(), newMessage.save()]);
 
-    await getConversation.save();
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     return res.status(200).json({
       message: "message sent sucessfully",
